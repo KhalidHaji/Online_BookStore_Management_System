3daf068fb9f6caa13ea18ee5a5f58d746fb5b1c7
haji <khalidhajilacag@gmail.com>
2023-01-11 12:27:40 +0600

    BookStoreMNGSYS

---
+++ admin/readme.txt
@@ -0,0 +1,67 @@
+Project: Online Book Store Management System
+
+About the Project
+____________________
+
+An online book store management system built using PHP, MySQL, HTML, and CSS is a system that allows users to browse and purchase books online. 
+It typically includes a catalog of available books, a shopping cart for purchasing multiple items, and a user system for managing orders and payments. 
+The system may also include features such as book reviews, ratings, and recommendations, as well as tools for managing inventory and processing orders. 
+The use of PHP, MySQL, HTML, and CSS allows the system to be dynamic and interactive, with a user-friendly interface that is easy to navigate.
+
+Importance
+_________________
+
+An online book store management system built using PHP, MySQL, HTML, and CSS can be important for several reasons. Some of the benefits of such a system include:
+
+*Increased convenience for customers: An online book store allows customers to browse and purchase books from the comfort of their own home, without having to physically visit a store.
+*Increased reach: An online book store can be accessed by anyone with an internet connection, regardless of their location. This allows the store to reach a wider audience than a physical store would.
+*Increased efficiency: An online book store management system can streamline the process of managing orders, payments, and inventory, allowing the store to operate more efficiently.
+*Increased sales: By making it easier for customers to purchase books, an online book store can potentially increase sales and revenue.
+*Increased competitiveness: An online book store can be a valuable tool for competing with other stores, both online and offline. 
+*It allows the store to offer a convenient and user-friendly shopping experience to customers.
+
+
+Features
+___________
+*Login Page
+The page where the system admin or user submit their system credential to access the data and functionalities of the clothing store management system.
+
+*Home Page
+The page where the system users will be redirected by default when logging into the system.
+
+*Suppliers Page
+The page where the admin manages the list of the store's suppliers.
+
+*POS (Point of Sale) Page
+​​​​​​​The page where the cashier, admin, or staff will encode the purchases of their customers.
+
+*Products Page
+​​​​​​​​​​​​​​The page where the list of products are listed and managed.
+
+*Sales Report Page
+​​​​​​​The page where the monthly sales report is shown and ready to print.​​​​​​​
+
+*Users Page
+​​​​​​​The page where the system admin manages the list of clothing store management system users.​​​​​​​
+The Online Book Store Management System was developed using HTML, PHP/MySQLi, CSS, JavaScript (jQuery/Ajax), and Bootstrap for the design. 
+The source code is fully functional and easy to modify or enhance. Follow the instruction below to have an actual experience using this simple project.
+
+How to Run
+____________
+
+*Download the source code and extract the zip file.
+*Download or set up any local web server that runs PHP script.
+*Open the web-server database and create a new database name it clothing_db.
+*Import the SQL file located in the database folder of the source code.
+*Copy and paste the source code to the location where your local web server accessing your local projects. Example for XAMPP('C:\xampp\htdocs')
+*Open a web browser and browse the project. E.g [http://localhost/clothing-store-management-system]
+
+Admin Default Access
+Username: admin
+Password: admin123
+
+Developer: Khalid Mohamed Abdi (Hajilacag)
+Email:khalidhajilacag@gmail.com
+LinkedIn:https://www.linkedin.com/in/khalid-mohamed-abdi-hajilacag-03b42a1b2/
+
+Enjoy!
---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal - www.codingnepalweb.com -->
+<html lang="en" dir="ltr">
+
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!------<title> Website Layout | CodingLab</title>------>
+    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+
+
+    <style type="text/css">
+    	#myVideo {
+         width: 100vw;
+  height: 100vh;
+  object-fit: cover;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  z-index: -1;
+}
+
+/* Add some content at the bottom of the video/page */
+.content {
+  position: fixed;
+  bottom: 0;
+  color: #f1f1f1;
+  width: 100%;
+  padding: 20px;
+}
+
+/* Style the button used to pause/play the video */
+#myBtn {
+  width: 200px;
+  font-size: 18px;
+  padding: 10px;
+  border: none;
+  background: #000;
+  color: #fff;
+  cursor: pointer;
+}
+
+#myBtn:hover {
+  background: #ddd;
+  color: black;
+}
+
+    </style>
+
+   </head>
+
+<body>
+	<!-- The video -->
+<video autoplay muted loop id="myVideo">
+  <source src="joyofbooks.mp4" type="video/mp4">
+</video>
+
+<!-- Optional: some overlay text to describe the video -->
+
+  <nav>
+    <div class="menu">
+      <div class="logo">
+        <a href="#">Online BookStore Management System</a>
+      </div>
+      <ul>
+        <li><a href="http://localhost/book_store/indexHore.php?page=home">Home</a></li>
+        <li><a href="http://localhost/book_store/indexHore.php?page=about">About</a></li>
+        <li><a href="http://localhost/book_store/indexHore.php?page=home">Services</a></li>
+        <li><a href="#">Contact</a></li>
+       
+      </ul>
+    </div>
+  </nav>
+  <div class="content">
+  	<div class="img"></div>
+  <div class="center">
+    <div style="color: #FFFF00;font-size: 55px;font-weight: 600;">“Take a good book to bed with you<br>books do not snore.”</div>
+    <div style="color:#00acee;font-size: 52px;font-weight: 600;">Pure and upto date Books are Available
+    </div>
+    <div class="btns">
+      <button><a style="text-decoration: none;" href="indexHore.php">Get Started!</a></button>
+      <button>Subscribe</button>
+    </div>
+  </div>
+
+ </div>
+ <div>
+ 	<footer style=" background-color: #006400;font-size: 2rem;margin-top: 98vh;padding: 50px;">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8 text-center">
+				<h2 style="text-align: center; color: white;font-size: 2rem;" class="mt-0 text-white">For more Information</h2>
+                        <h2 style="text-align: center;color: white;font-size: 2rem;" class="mt-0 text-white">Contact us</h2>
+<hr style="max-width: 3.5rem;border-width:0.2rem;box-sizing: content-box;height: 0;overflow: visible;margin-left: 50%;"/>
+                    </div>
+                </div>
+                <div class="row">
+                    <div style="margin-left: 30%;  font-size: 20px; color:whitesmoke;margin-top: 30px;" >
+                        <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
+                        <div  class="text-white">+8801829063365</div>
+                    </div>
+                    <div >
+                      <span style="margin-left: 56%; font-size: 20px;">
+                        <i  class="fas fa-envelope fa-3x mb-3 text-muted"></i>
+                        <!-- Make sure to change the email address in BOTH the anchor text and the link target below!-->
+                        <a  href="#" class="d-block">khalidhajilacag@gmail.com</a></span>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div style="text-align:center; font-size: 1.5rem;"class="container"><div style="color:white;" class="small text-center text-muted">Copyright © 2022 - Haji Online Book Store | <a href="https://www.khalidhajilacag.com/" target="_blank">Khalid Mohamed Abdi</a></div></div>
+        </footer>
+        
+       <?php include('footer.php') ?>
+ </footer>
+ </div>
+
+</body>
+</html>
